8ceea1d1655c1d1f6b9cf9144b9b6dce80e91d24
Nick Mallas <nmallas23@gmail.com>
2021-04-08 11:47:21 -0700

    Create Question Model

---
+++ backend/models/question.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const QuestionSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    repo: {
+        type: String
+    },
+    live: {
+        type: String
+    },
+    answered: {
+        type: Boolean,
+        default: false
+    }
+}, {timeStamps: true})
+
+module.exports = Question = mongoose.model("Question", QuestionSchema)
