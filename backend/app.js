9754ca1c95e3338931e223afcf1cb7d1f35a46bd
Nick Mallas <nmallas23@gmail.com>
2021-04-08 11:33:47 -0700

    Set up mongoDb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const db = mongoose()
+const db = require("./config/keys").mongoURI;
+
+mongoose
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> console.log("Mongo running"))
+    .catch(err => console.log(err))
 
 app.get("/", (req, res) => {
     res.send("Home Page")
