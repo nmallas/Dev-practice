5d77b9905f7e95f84d72503b4e58ea6a36166860
Nick Mallas <nmallas23@gmail.com>
2021-04-08 12:35:14 -0700

    add get and delete question routes

--- backend/routes/api/questions.js
+++ backend/routes/api/questions.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const Question = require("../../models/question");
 
+router.get("/", (req, res) => {
+    Question.find()
+        .then(questions => res.json(questions))
+        .catch(err => res.status(404).json(err))
+})
+
 router.post("/", (req, res)=> {
     const newQuestion = new Question({
         name: req.body.name,
@@ -14,6 +20,12 @@ router.post("/", (req, res)=> {
         .save()
         .then(question => res.json(question))
         .catch(err => res.status(404).json(err));
+})
+
+router.delete("/:questionId", (req, res) => {
+    Question.findOneAndDelete({_id: req.params.questionId})
+        .then(questions => res.json({_id: req.params.questionId}))
+        .catch(err => res.status(404).json(err))
 })
 
 module.exports = router;
